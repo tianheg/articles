d5a0985d58aae13a15f588b198b0b449292f2fec
tianheg <yidajiabei@163.com>
2021-06-15 07:34:13 +0800

    feat(new): insert h1 title while ceating file

--- new.sh
+++ new.sh
@@ -2,6 +2,7 @@
 
 echo -n 'Enter the Article Name: '
 read -r a
-touch 2021/"$(date +"%F")-$a.md" && code $_
+echo "# $a" >> 2021/"$(date +'%F')-$a.md"
+code 2021/"$(date +'%F')-$a.md"
 # 问题：
 # 如果文章名字含有空格，能够新建文章，但同时会新建其他文档
